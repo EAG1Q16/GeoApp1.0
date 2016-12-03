747b21e6cc57a59fd82f26192e2d8a111cad9a75
martacou <marta.couto.g@gmail.com>
2016-12-03 12:41:28 +0100

    Perfil aventura

--- www/js/controllers/AdventureCtrl.js
+++ www/js/controllers/AdventureCtrl.js
@@ -7,20 +7,81 @@ app.controller('AdventureCtrl', function ($scope, $ionicPopup, $http, $rootScope
   var adventureID = window.location.href.split("/").pop();
   console.log("AdventureID: " + adventureID);
 
+  //Obtener los datos para mostrar detalles de una aventura
   $http.get(base_url+'/adventures/id/' + adventureID)
     .success(function(data){
       $scope.AdventureProfile = data;
       $scope.comments = data.comments;
+      angular.forEach($scope.comments, function (comment, key) {
+        console.log(key);
+        comment.commentdate = moment(comment.commentdate, "").fromNow();
+      });
       console.log($scope.AdventureProfile);
       console.log($scope.comments);
     })
     .error(function (data) {
       console.log("Error: " + data);
+    });
+
+  // Saber si la aventura esta en favs o no
+  $http.get(base_url+'/user/isadvfav/' + adventureID +'/'+ $rootScope.UserID)
+    .success(function(data) {
+      console.log(data);
+      $scope.isfav = data;
+      console.log("isfav: " + $scope.isfav);
     })
+    .error(function(data) {
+      console.log('Error: ' + data);
+    });
 
+  //FAV y UNFAV
+  $scope.FavAdventure = function () {
+      $http.post(base_url+'/user/afavadv/' + adventureID, $rootScope.UserID)
+        .success(function(data){
+          $scope.AdventureProfile = data;
+          $http.get(base_url+'/user/isadvfav/' + adventureID +'/'+ $rootScope.UserID)
+            .success(function(data) {
+              console.log(data);
+              $scope.isfav = data;
+              console.log($scope.isfav);
+            })
+            .error(function(data) {
+              console.log('Error: ' + data);
+            });
+
+        })
+        .error(function(data) {
+          console.log('Error' + data);
+        });
+  };
+
+  $scope.UnfavAdventure = function () {
+    $http.delete(base_url+'/user/uafavadv/' + adventureID +'/'+ $rootScope.UserID)
+      .success(function(data){
+        $scope.AdventureProfile = data;
+        $http.get(base_url+'/user/isadvfav/' + adventureID +'/'+ $rootScope.UserID)
+          .success(function(data) {
+            console.log(data);
+            $scope.isfav = data;
+            console.log($scope.isfav);
+          })
+          .error(function(data) {
+            console.log('Error: ' + data);
+          });
+
+      })
+      .error(function(data) {
+        console.log('Error' + data);
+      });
+  };
+
+  //Tweet
+  $scope.AddTweet = function () {
+
+  };
+
+  //Comentar una aventura
   $scope.NewComment={};
-
-  //$scope.NewCommenr.user = $rootScope.UserID;
   $scope.PostComment = function () {
     $http.post(base_url+'/comments/' + $rootScope.UserID, $scope.NewComment)
       .success(function(data) {
@@ -33,6 +94,10 @@ app.controller('AdventureCtrl', function ($scope, $ionicPopup, $http, $rootScope
           .success(function (data) {
             console.log("entro en el success");
             $scope.comments = data.comments;
+            angular.forEach($scope.comments, function (comment, key) {
+              console.log(key);
+              comment.commentdate = moment(comment.commentdate, "").fromNow();
+            });
             console.log("aqui");
           })
           .error(function (data) {
@@ -42,6 +107,33 @@ app.controller('AdventureCtrl', function ($scope, $ionicPopup, $http, $rootScope
       .error(function (data) {
         console.log("Error" + data);
       });
+  };
+
+  //Eliminar un comentario
+  $scope.deletecomment = function (cmd_id) {
+    var ConfirmPopup = $ionicPopup.confirm({
+      title: 'Esta seguro que quiere eliminar este comentario?'
+    });
+    ConfirmPopup.then(function (res) {
+      if(res){
+        $http.delete(base_url+'/comments/deletecomment/' + cmd_id + '/' + adventureID)
+          .success(function (data) {
+            console.log("deletecomment");
+            $scope.comments = data.comments;
+            angular.forEach($scope.comments, function (comment, key) {
+              console.log("FOREACH");
+              console.log(key);
+              comment.commentdate = moment(comment.commentdate, "").fromNow();
+            });
+          })
+          .error(function (data) {
+            console.log("Error" + data);
+          });
+      } else {
+        console.log("No eliminas el comentario");
+      }
+    })
+
   };
 
 })
