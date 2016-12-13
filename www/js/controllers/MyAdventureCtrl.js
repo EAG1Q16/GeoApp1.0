1ca3c7b8dfbed070aacb406405d54b1be82b79e6
martacou <marta.couto.g@gmail.com>
2016-12-13 16:01:12 +0100

    mis aventuras

--- www/js/controllers/MyAdventureCtrl.js
+++ www/js/controllers/MyAdventureCtrl.js
@@ -2,24 +2,59 @@
  * Created by Marta_ on 28/11/2016.
  */
 
-
-
 app.controller('MyAdventureCtrl', function ($scope, $rootScope, $http, $ionicPopup, $stateParams, $timeout,  $state){
   console.log("rootscope: "+$rootScope.UserID);
+
+  $scope.Bcreated = 'false';
+  $scope.Bplayed = 'true';
+  $scope.Bfavs = 'false';
+
   $http.get(base_url+'/user/my/'+$rootScope.UserID)
     .success(function (response) {
-      $scope.adventures = response.adventures.created;
-      console.log(response);
-      console.log(response.adventures.created);
+      $scope.items = response.adventures.played;
     })
     .error(function (response) {
       console.log("Error: "+response);
     })
 
   $scope.showCreadas = function(){
+    $http.get(base_url+'/user/my/'+$rootScope.UserID)
+      .success(function (response) {
+        $scope.items = response.adventures.created;
+        $scope.Bplayed = 'false';
+        $scope.Bcreated = 'true';
+        $scope.Bfavs = 'false';
+      })
+      .error(function (response) {
+        console.log("Error: "+response);
+      })
 
   }
   $scope.showJugadas = function(){
+    $http.get(base_url+'/user/my/'+$rootScope.UserID)
+      .success(function (response) {
+        $scope.items = response.adventures.played;
+        $scope.Bplayed = 'true';
+        $scope.Bcreated = 'false';
+        $scope.Bfavs = 'false';
+      })
+      .error(function (response) {
+        console.log("Error: "+response);
+      })
 
   }
+  $scope.showFav = function(){
+    $http.get(base_url+'/user/my/'+$rootScope.UserID)
+      .success(function (response) {
+        $scope.items = response.adventures.favs;
+        $scope.Bplayed = 'false';
+        $scope.Bcreated = 'false';
+        $scope.Bfavs = 'true';
+      })
+      .error(function (response) {
+        console.log("Error: "+response);
+      })
+
+  }
+
 })
