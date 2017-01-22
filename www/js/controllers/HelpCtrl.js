d9e2a5c83f81a544d72ca9f33c5f918586d281c6
martacou <marta.couto.g@gmail.com>
2017-01-22 12:34:23 +0100

    cositas bonitas

--- www/js/controllers/HelpCtrl.js
+++ www/js/controllers/HelpCtrl.js
@@ -4,6 +4,22 @@
 
 app.controller('HelpCtrl', function ($scope, $rootScope, $ionicModal, $ionicPopup, $http, $stateParams, $timeout, $state) {
 
+  // Called to navigate to the main app
+  $scope.startApp = function() {
+    $state.go('main');
+  };
+  $scope.next = function() {
+    $ionicSlideBoxDelegate.next();
+  };
+  $scope.previous = function() {
+    $ionicSlideBoxDelegate.previous();
+  };
+
+  // Called each time the slide changes
+  $scope.slideChanged = function(index) {
+    $scope.slideIndex = index;
+  };
+
   $ionicModal.fromTemplateUrl('templates/sugerencias.html', {
     scope: $scope
   }).then(function(modal) {
